51d3b5b9ed04910837672b51c613de76d563a42f
Fyodor76 <100220038+Fyodor76@users.noreply.github.com>
2024-05-26 00:46:44 +0300

    Добавлены стили для services

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.scss'
 import { Header } from './stages/header/Header'
+import { Services } from './stages/services/Services'
 
 function App() {
   return (
     <div className='main'>
       <Header/>
+      <Services/>
     </div>
   )
 }
---
+++ src/stages/services/Services.tsx
@@ -0,0 +1,18 @@
+
+export const Services = () => {
+    return (
+        <div className="services">
+            <div className="first-services-block">
+                <div className="services-header">
+                    <div>
+                         <h1>Услуги</h1>
+                    </div>
+                </div>
+                <div className="services-container"></div>
+            </div>
+            <div className="second-services-block">
+                <div className="services-container"></div>
+            </div>
+        </div>
+    )
+}
