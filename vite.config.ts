0a7062ac87298a8b90c46d585162e107401b45b0
Fyodor76 <100220038+Fyodor76@users.noreply.github.com>
2024-05-26 13:43:39 +0300

    изменения в vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-      react(),
-  ],
-  base: 'studio-master-classes',
+  plugins: [react()],
+  base: process.env.NODE_ENV === 'production' && process.env.VITE_ENV === 'test'
+    ? '/studio-master-classes/test/'
+    : '/studio-master-classes/',
 });
