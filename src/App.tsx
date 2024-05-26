115a4d03065bf90270c4976b2ffebcefae7ce94b
Fyodor76 <100220038+Fyodor76@users.noreply.github.com>
2024-05-26 14:02:23 +0300

    добавлен console.log

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from './stages/header/Header'
 import { Services } from './stages/services/Services'
 
 function App() {
+  console.log('test')
   return (
     <div className='main'>
       <Header/>
